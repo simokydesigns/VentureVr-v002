6a332c2140acfe89cb43cf2f7c2fad03c3e9c127
Simon <simok@eircom.net>
2017-11-24 11:44:45 +0000

    refactoring

---
+++ venturevr-app/Venture/WebContent/webapp/service/init.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDbJFi2S9I3OR7_zMApGnQxw-wT6Ity1qc",
+  authDomain: "venturevr-ef771.firebaseapp.com ",
+  databaseURL: "https://venturevr-ef771.firebaseio.com",
+  storageBucket: "venturevr-ef771.appspot.com",
+  messagingSenderId: "567715903006"
+};
+firebase.initializeApp(config);
